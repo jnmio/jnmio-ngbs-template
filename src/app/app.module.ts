d61a8fd013db538726004f43ce324bb76450362d
jnmdev <j@jnmdev.com>
2022-10-23 14:33:35 -0500

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { SharedModule } from './shared/shared.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -9,7 +10,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
+    CommonModule,
     BrowserModule,
+    SharedModule.forRoot(),
     AppRoutingModule
   ],
   providers: [],
